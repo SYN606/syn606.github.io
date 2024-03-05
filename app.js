bbc5bd9c89153105b4fd767a6c15e5e9ec6bea58
SYN <syn.vik666@yandex.com>
2024-03-05 18:47:03 +0530

    Navbar created

---
+++ app.js
@@ -0,0 +1,12 @@
+const menuBtn = document.querySelector("#menu");
+const menuContent = document.querySelector("#menu-content");
+
+menuBtn.addEventListener("click", () => {
+    menuContent.classList.toggle("hidden");
+    if (menuContent.classList != "hidden") {
+        menuBtn.innerHTML = '<i class="fa-brands fa-xing"></i>'
+    }
+    else if (menuContent.classList == "hidden") {
+        menuBtn.innerHTML = '<i class="fa-solid fa-bars-staggered"></i>'
+    }
+})
