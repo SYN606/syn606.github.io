bf9f3f6bf406eb8d8753b4657ac19f016e1d2818
SYN606 <postbox.501@yandex.com>
2024-06-23 18:36:05 +0530

    added schema markup

--- src/script.js
+++
@@ -1,9 +0,0 @@
-let deploymentBtn = document.querySelectorAll('.deployment');
-
-console.log(deploymentBtn)
-
-deploymentBtn.forEach(element => {
-    element.addEventListener('click', () => {
-        alert("hello world");
-    });
-});
